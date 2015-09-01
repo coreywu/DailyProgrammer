f188b3000440b60bbbb44af78bb48bdc2e5466c9
Corey Wu <coreywu0@gmail.com>
2015-09-01 14:34:52 -0400

    Add more efficient divisibility check

--- 229_Divisible_by_7/DivisibleBy7Graph.scala
+++ 229_Divisible_by_7/DivisibleBy7Graph.scala
@@ -25,21 +25,61 @@ object DivisibleBy7Graph {
     isDivisibleBy7(newNumber, newNode)
   }
   
-  def main(args: Array[String]) {
+  def isReverseDivisibleBy7(number: Long): Boolean = {
+    isReverseDivisibleBy7(number.toString(), 0)
+  }
+
+  private def isReverseDivisibleBy7(number: String, node: Int): Boolean = {
+    val firstDigit = Integer.parseInt(number.charAt(number.length() - 1).toString(), 10)
+    val newNumber = number.substring(0, number.length() - 1)
+    var newNode = (node + firstDigit) % 7
+    if (newNumber.size == 0) {
+      return newNode == 0
+    } else {
+      newNode = nextNodeArray(newNode)
+    }
+    isReverseDivisibleBy7(newNumber, newNode)
+  }
     
+  def isReverseDivisibleBy7Efficient(number: Long): Boolean = {
+    var currentNumber = number
+    var node: Int = 0
+    while (currentNumber >= 10) {
+      val firstDigit = (currentNumber % 10).toInt
+      currentNumber = currentNumber / 10
+      node = (node + firstDigit) % 7
+      node = nextNodeArray(node)
+    }
+    node = (node + currentNumber.toInt) % 7
+    node == 0
+  }
+  
+  
+  def main(args: Array[String]) {
+    println(isReverseDivisibleBy7Efficient(14))
+    println(isReverseDivisibleBy7Efficient(41))
+    println(isReverseDivisibleBy7Efficient(7777777))
+    println(isReverseDivisibleBy7Efficient(1111))
+    println(isReverseDivisibleBy7Efficient(14984001))
+    println(isReverseDivisibleBy7Efficient(34048584342L))
+    
+    val start = System.nanoTime   
+
     var number = 1001L
     while (number <= Math.pow(10, 9)) {
       if (reversedSet.contains(number)) {
         resultSet +:= number
       } else {
-    	  val reverseNumber = number.toString().reverse.toLong
-    		if (isDivisibleBy7(reverseNumber)) {
-    		  resultSet +:= number
-    		  reversedSet += reverseNumber
-    		}
+        if (isReverseDivisibleBy7Efficient(number)) {
+          resultSet +:= number
+          val reverseNumber = number.toString().reverse.toLong
+          reversedSet += reverseNumber
+        }
       }
       number += 7
     }
+    
+    println((System.nanoTime - start) / 1000000000 + " secs")
     
     println("Result size: " + resultSet.size)
     println("Result sum: " + resultSet.sum)

4953c5f2fa9d2ef35bb43cdce6eb459e10832565
Corey Wu <coreywu0@gmail.com>
2015-11-18 01:41:22 -0500

    Add comments for #214 Pile of Paper

--- 214_Pile_of_Paper/Pile_of_Paper.py
+++ 214_Pile_of_Paper/Pile_of_Paper.py
@@ -1,4 +1,7 @@
 class Paper(object):
+    """Class that represents a single piece of paper with attributes for the 
+    colour, x and y positions of top left corner, width and height.
+    """
     def __init__(self, paper_line):
         self.colour = int(paper_line[0])
         self.x = int(paper_line[1])
@@ -7,11 +10,19 @@ class Paper(object):
         self.height = int(paper_line[4])
 
     def fully_covers(self, old_paper):
+        """Returns whether this piece of paper fully covers the given piece wit
+        the provided x, y, width and height attributes.
+        """
         return (self.x <= old_paper.x and self.y <= old_paper.y and 
                 self.x + self.width >= old_paper.x + old_paper.width and 
                 self.y + self.height >= old_paper.y + old_paper.height)
             
     def delta(self, width, height):
+        """Returns the "delta" of the piece of paper, which is a 2d array which
+        represents the area, with 1s in the position where the piece of paper 
+        occupies. This delta is used to compute differences in the overlapping
+        areas of two pieces of paper.
+        """
         delta = [[0 for i in range(width)] for j in range(height)]
         for y in range(self.y, self.y + self.height):
             for x in range(self.x, self.x + self.width):
@@ -24,6 +35,9 @@ class Paper(object):
 
 
 def compute_delta(delta1, delta2):
+    """Returns the difference in delta of two pieces of paper. The difference
+    is the first piece of paper's delta with 0s wherever the two overlap.
+    """
     delta = list(delta1)
     for y in range(len(delta1)):
         for x in range(len(delta1[0])):
@@ -33,6 +47,8 @@ def compute_delta(delta1, delta2):
     return delta
 
 def is_empty(delta):
+    """Returns whether the delta is entirely 0s, indicating no visible paper.
+    """
     for y in range(len(delta)):
         for x in range(len(delta[0])):
             if delta[y][x] == 1:
@@ -40,6 +56,8 @@ def is_empty(delta):
     return True
 
 def count_spaces(delta):
+    """Returns the number of 1s in the delta.
+    """
     spaces = 0
     for y in range(len(delta)):
         for x in range(len(delta[0])):
@@ -48,6 +66,9 @@ def count_spaces(delta):
     return spaces
 
 def read_from_file(filename):
+    """Reads the attributes of a pile of paper and the area, generating a stack
+    of papers which we will traverse backwards.
+    """
     stack = []
     with open(filename) as f:
         width_height_line = f.readline().split(" ")
@@ -60,6 +81,13 @@ def read_from_file(filename):
     return grid_width, grid_height, stack
 
 def pile_of_paper(filename):
+    """The algorithm essentially builds a stack of papers in order to traverse
+    from the top most piece of paper down. Every piece of paper after the first
+    is checked to determine if it is visible by calculating the delta of that
+    piece with all other visibile papers. If it is, decrement `unfilled_spaces`
+    by the area covered. This is continued until either all papers are
+    accounted for, or all of the area is covered.
+    """
     width, height, paper_stack = read_from_file(filename)
     unfilled_spaces = width * height
 

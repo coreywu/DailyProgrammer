f1f45c5f1d20281c51feb90fc50c01171db4b218
Corey Wu <coreywu0@gmail.com>
2015-11-20 19:28:57 -0500

    Complete #211 Ogre Maze with comments

--- 211_Ogre_Maze/Ogre_Maze.kt
+++ 211_Ogre_Maze/Ogre_Maze.kt
@@ -10,11 +10,21 @@ import java.util.Queue
 data class Point(val x: Int, val y: Int)
 data class Path(val position: Point, val visitedList: ArrayList<Point>)
 
+/**
+ * Returns a list of valid paths that can be reached by moving the ogre one step
+ * up, down, left or right. Checks for bounds and sinkholes. Does not move back
+ * to a previously visited location.
+ *
+ * @param path the current Path used to reach this position.
+ * @param map the 2d array representing the map.
+ * @param visitedSet the set of Points visited so far by the algorithm.
+ * @return a list of Paths which contain the valid, new adjacent positions.
+ */
 fun getValidAdjacentPaths(path: Path, map: Array<CharArray>, visitedSet: HashSet<Point>): List<Path> {
-    val adjacentPaths: ArrayList<Path> = ArrayList<Path>()
+    val adjacentPaths = ArrayList<Path>()
 
     // Go up
-    if (path.position.y > 1) {
+    if (path.position.y > 0) {
         val newPosition = Point(path.position.x, path.position.y - 1)
         if (!visitedSet.contains(newPosition)) {
             val newPath = checkValidPositionAndCreatePath(newPosition, path.visitedList, map)
@@ -23,7 +33,7 @@ fun getValidAdjacentPaths(path: Path, map: Array<CharArray>, visitedSet: HashSet
     }
 
     // Go down
-    if (path.position.y < map.size - 1) {
+    if (path.position.y < map.size - 2) {
         val newPosition = Point(path.position.x, path.position.y + 1)
         if (!visitedSet.contains(newPosition)) {
             val newPath = checkValidPositionAndCreatePath(newPosition, path.visitedList, map)
@@ -41,7 +51,7 @@ fun getValidAdjacentPaths(path: Path, map: Array<CharArray>, visitedSet: HashSet
     }
 
     // Go right
-    if (path.position.x < map[0].size - 1) {
+    if (path.position.x < map[0].size - 2) {
         val newPosition = Point(path.position.x + 1, path.position.y)
         if (!visitedSet.contains(newPosition)) {
             val newPath = checkValidPositionAndCreatePath(newPosition, path.visitedList, map)
@@ -52,8 +62,20 @@ fun getValidAdjacentPaths(path: Path, map: Array<CharArray>, visitedSet: HashSet
     return adjacentPaths
 }
 
+/**
+ * Returns a Path if the new position of the ogre is valid (not on a sinkhole)
+ * or returns null otherwise. The function checks the current position, the
+ * position at (x + 1, y), the position at (x, y + 1) and finally at
+ * (x + 1, y + 1). These four coordinates represent the position of the ogre.
+ *
+ * @param newPosition the new position of the ogre to check.
+ * @param visitedList a list of Points traversed on the path.
+ * @param map the 2d array representing the map.
+ * @return a Path if there is no sinkhole at the ogre's 2x2 position else null.
+ */
 fun checkValidPositionAndCreatePath(newPosition: Point, visitedList: ArrayList<Point>, map: Array<CharArray>): Path? {
-    if (map[newPosition.x][newPosition.y] != 'O' && map[newPosition.x][newPosition.y] != 'O') {
+    if (map[newPosition.y][newPosition.x] != 'O' && map[newPosition.y + 1][newPosition.x] != 'O'
+            && map[newPosition.y][newPosition.x + 1] != 'O' && map[newPosition.y + 1][newPosition.x + 1] != 'O') {
         val newVisitedList = ArrayList(visitedList)
         newVisitedList.add(newPosition)
         return Path(newPosition, newVisitedList)
@@ -61,6 +83,14 @@ fun checkValidPositionAndCreatePath(newPosition: Point, visitedList: ArrayList<P
     return null
 }
 
+/**
+ * Returns whether or not the current position is the "goal"
+ *
+ * @param position the position to check.
+ * @param goal the position of the goal.
+ * @param map the 2d array representing the map.
+ * @return a Boolean for whether the current position is equal to the goal.
+ */
 fun goalPosition(position: Point, goal: Point, map: Array<CharArray>): Boolean {
     return (position.equals(goal))
         || (position.x < map[0].size - 1 && position.x + 1 == goal.x && position.y == goal.y)
@@ -68,9 +98,34 @@ fun goalPosition(position: Point, goal: Point, map: Array<CharArray>): Boolean {
         || (position.x < map[0].size - 1 && position.y < map.size - 1 && position.x + 1 == goal.x && position.y + 1 == goal.y)
 }
 
-fun main(args: Array<String>) {
+fun formatSolution(solution: ArrayList<Point>, map: Array<CharArray>): Array<CharArray> {
+    for (point in solution) {
+        map[point.y][point.x] = '&'
+        map[point.y][point.x + 1] = '&'
+        map[point.y + 1][point.x] = '&'
+        map[point.y + 1][point.x + 1] = '&'
+    }
+    return map
+}
+
+fun printMap(map: Array<CharArray>) {
+    for (row in map) {
+        for (char in row) {
+            print(char)
+        }
+        print('\n')
+    }
+}
+
+/**
+ * Algorithm uses BFS with an early exit to find the shortest path to the
+ * goal, or "No path found" if no path exists.
+ *
+ * @param filename the name of the file used to read information about the map.
+ */
+fun findPath(filename: String) {
     val inputList = ArrayList<String>()
-    File("input1.txt").forEachLine { inputList.add(it) }
+    File(filename).forEachLine { inputList.add(it) }
     val map: Array<CharArray> = Array(inputList.size, { CharArray(inputList[0].length)})
 
     var start: Point? = null
@@ -101,13 +156,14 @@ fun main(args: Array<String>) {
 
     val queue: Queue<Path> = LinkedList<Path>()
     var solution: ArrayList<Point>? = null
-    val visitedSet: HashSet<Point> = HashSet<Point>()
+    val visitedSet = HashSet<Point>()
     var done: Boolean = false
 
     queue.add(Path(start, arrayListOf(start)))
 
     while (!done && queue.isNotEmpty()) {
         val path = queue.remove()
+        visitedSet.add(path.position)
         if (goalPosition(path.position, goal, map)) {
             solution = path.visitedList
             done = true
@@ -115,5 +171,20 @@ fun main(args: Array<String>) {
         queue.addAll(getValidAdjacentPaths(path, map, visitedSet))
     }
 
-    println(solution)
+    if (solution != null) {
+        printMap(formatSolution(solution, map))
+    } else {
+        println("No Path")
+    }
+
+    println("\n\n")
+}
+
+fun main(args: Array<String>) {
+    findPath("input1.txt")
+    findPath("input2.txt")
+    findPath("challenge_input1a.txt")
+    findPath("challenge_input1b.txt")
+    findPath("challenge_input1c.txt")
+    findPath("challenge_input2.txt")
 }
